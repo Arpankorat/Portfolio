7c4f6adb79b1dd2706ad054f42e727cbb4c23e7b
Arpan Korat <arpankorat@gmail.com>
2023-07-21 12:22:18 +0530

    Updates

---
+++ precache-manifest.a068b4cc439a5535f49697fa3235f363.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b2084f0d70a754442ae94b3028feb6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fe048c3acd503d9e396",
+    "url": "/static/css/2.e44df365.chunk.css"
+  },
+  {
+    "revision": "9020e1df514d573cd415",
+    "url": "/static/css/main.de0ddc33.chunk.css"
+  },
+  {
+    "revision": "0fe048c3acd503d9e396",
+    "url": "/static/js/2.2718d8fa.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2718d8fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9020e1df514d573cd415",
+    "url": "/static/js/main.8287fe2c.chunk.js"
+  },
+  {
+    "revision": "bd39f8486317ba5e0448",
+    "url": "/static/js/runtime-main.7bd266ea.js"
+  },
+  {
+    "revision": "c91e307d918cf699a20535b2f450e73c",
+    "url": "/static/media/close.c91e307d.svg"
+  },
+  {
+    "revision": "d150d4d60b034a7b8fc0df36dd389590",
+    "url": "/static/media/github.d150d4d6.svg"
+  },
+  {
+    "revision": "e5f5d20af21cf701bee9a33bbdb351e8",
+    "url": "/static/media/instagram.e5f5d20a.svg"
+  },
+  {
+    "revision": "0e1dbd0a18f765b0d1ad0b89844488d9",
+    "url": "/static/media/linkedin.0e1dbd0a.svg"
+  },
+  {
+    "revision": "294957c3bddca6a29c6f34fe668c0392",
+    "url": "/static/media/logo.294957c3.png"
+  },
+  {
+    "revision": "81808241e21e51c4e9fa9156ba127eab",
+    "url": "/static/media/menu.81808241.svg"
+  },
+  {
+    "revision": "f70ca2cfc49ccb3f4829666b74e9a6a2",
+    "url": "/static/media/profile.f70ca2cf.jpeg"
+  },
+  {
+    "revision": "66a49535097afc53b7f0d33c32811c9d",
+    "url": "/static/media/twitter.66a49535.svg"
+  }
+]);
